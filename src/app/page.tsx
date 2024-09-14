468b8169f6e85ba7175d1bf68f15f02fc10fc47c
Batikan-Kutluer <60034174+Batikan-Kutluer@users.noreply.github.com>
2024-09-14 16:08:05 +0300

    ITU-GPA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,229 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import matches from "./matches.json";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [userYKS, setUserYKS] = useState("");
+  const [results, setResults] = useState<any[]>([]);
+  const [showResults, setShowResults] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setResults(matches);
+    setShowResults(true);
+  };
+
+  return (
+    <div className="container mx-auto px-4 py-8 bg-slate-600 min-h-screen">
+      <h1 className="text-3xl font-bold mb-6 text-amber-400 text-center">
+        İTÜ GPA Hesaplama
+      </h1>
+      <form onSubmit={handleSubmit} className="mb-8 flex justify-center">
+        <input
+          type="number"
+          min="0"
+          max="560"
+          value={userYKS}
+          onChange={(e) => setUserYKS(e.target.value)}
+          placeholder="Enter a value (0-560)"
+          className="w-48 border text-black border-gray-300 rounded px-4 py-2 mr-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        />
+        <button
+          type="submit"
+          className="bg-slate-800 hover:bg-amber-700 hover:text-amber-400 text-slate-300 font-bold px-6 py-2 rounded transition duration-300"
+          disabled={
+            userYKS === "" || Number(userYKS) < 0 || Number(userYKS) > 560
+          }
+        >
+          Process
+        </button>
+      </form>
+      {showResults && (
+        <div>
+          <div className="overflow-x-auto bg-slate-600 rounded-lg shadow-md">
+            <table className="min-w-full">
+              <thead>
+                <tr className="bg-gray-200 text-gray-700 border-2 border-slate-400">
+                  <th className="py-3 px-4 text-left bg-slate-800 text-slate-200">
+                    Program
+                  </th>
+                  <th className="py-3 px-4 text-center">Dönem</th>
+                  <th className="py-3 px-4 text-center">Kontenjan</th>
+                  <th className="py-3 px-4 text-center">Yerleşen</th>
+                  <th className="py-3 px-4 text-center">Max GPA</th>
+                  <th className="py-3 px-4 text-center">Min GPA</th>
+                </tr>
+              </thead>
+
+              {results.map((program, index) => {
+                if (program?.fakulte) return;
+                return (
+                  <>
+                    <Row
+                      key={program?.programAdi + "3.Yarıyıl" + index}
+                      program={program}
+                      yilAdi="3.Yarıyıl"
+                      userYKS={userYKS}
+                    />
+                    <Row
+                      key={program?.programAdi + "5.Yarıyıl" + index}
+                      program={program}
+                      yilAdi="5.Yarıyıl"
+                      userYKS={userYKS}
+                    />
+                  </>
+                );
+              })}
+            </table>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      )}
     </div>
+  );
+}
+
+function Row({
+  program,
+  yilAdi,
+  userYKS,
+}: {
+  program: any;
+  yilAdi: string;
+  userYKS: string;
+}) {
+  let yil = program?.yil[yilAdi];
+
+  let kontenjan = yil?.kontenjan || "-";
+  let yerlesen = yil?.yerlesen || "-";
+  let maxSkor = yil?.maxSkor || "-";
+  let minSkor = yil?.minSkor || "-";
+
+  let yksTabanPuan = program?.yksTabanPuan.replace(",", ".");
+
+  let minGPA: any = "-";
+  let maxGPA: any = "-";
+
+  if (program.programAdi == "Kontrol ve Otomasyon Mühendisliği")
+    yksTabanPuan = 524.44;
+  if (program.programAdi == "Meteoroloji Mühendisliği") yksTabanPuan = 432.85;
+
+  if (minSkor !== "-" && yksTabanPuan !== "-") {
+    minGPA =
+      (minSkor * yksTabanPuan - Number(userYKS) * 0.4) / (yksTabanPuan * 0.15);
+  }
+
+  if (maxSkor !== "-" && yksTabanPuan !== "-") {
+    maxGPA =
+      (maxSkor * yksTabanPuan - Number(userYKS) * 0.4) / (yksTabanPuan * 0.15);
+  }
+
+  if (program.programAdi == "Maden Mühendisliği") {
+    console.log(minGPA, maxGPA);
+    console.log(minSkor, maxSkor);
+    console.log(minSkor !== "-", yksTabanPuan !== "-");
+  }
+
+  if (program?.sadeceIngilizceVar)
+    return <RowIngilizce program={program} yilAdi={yilAdi} userYKS={userYKS} />;
+  return (
+    <RowTemplate
+      programAdi={program.programAdi}
+      yilAdi={yilAdi}
+      kontenjan={kontenjan}
+      yerlesen={yerlesen}
+      minGPA={minGPA}
+      maxGPA={maxGPA}
+      isIngilizce={false}
+    />
+  );
+}
+
+function RowIngilizce({
+  program,
+  yilAdi,
+  userYKS,
+}: {
+  program: any;
+  yilAdi: string;
+  userYKS: string;
+}) {
+  let yil = program?.yil[yilAdi];
+
+  let kontenjan = yil?.ingilizce?.kontenjan || "-";
+  let yerlesen = yil?.ingilizce?.yerlesen || "-";
+  let maxSkor = yil?.ingilizce?.maxSkor || "-";
+  let minSkor = yil?.ingilizce?.minSkor || "-";
+
+  let ingilizceYksTaban = program?.ingilizceYksPuani.replace(",", ".");
+
+  let minGPA: any = "-";
+  let maxGPA: any = "-";
+
+  if (minSkor !== "-" && ingilizceYksTaban !== "-") {
+    minGPA =
+      (minSkor * ingilizceYksTaban - Number(userYKS) * 0.4) /
+      (ingilizceYksTaban * 0.15);
+  }
+
+  if (maxSkor !== "-" && ingilizceYksTaban !== "-") {
+    maxGPA =
+      (maxSkor * ingilizceYksTaban - Number(userYKS) * 0.4) /
+      (ingilizceYksTaban * 0.15);
+  }
+
+  return (
+    <RowTemplate
+      programAdi={program.programAdi}
+      yilAdi={yilAdi}
+      kontenjan={kontenjan}
+      yerlesen={yerlesen}
+      minGPA={minGPA}
+      maxGPA={maxGPA}
+      isIngilizce={true}
+    />
+  );
+}
+
+function RowTemplate({
+  programAdi,
+  yilAdi,
+  kontenjan,
+  yerlesen,
+  minGPA,
+  maxGPA,
+  isIngilizce,
+}: {
+  programAdi: string;
+  yilAdi: string;
+  kontenjan: string;
+  yerlesen: string;
+  minGPA: string;
+  maxGPA: string;
+  isIngilizce: boolean;
+}) {
+  if (minGPA == "-") minGPA = "Girersiniz";
+  else minGPA = minGPA.toString().slice(0, 4);
+  if (maxGPA == "-") maxGPA = "Girersiniz";
+  else maxGPA = maxGPA.toString().slice(0, 4);
+
+  if (Number(minGPA) > 4) minGPA = "4>";
+  if (Number(maxGPA) > 4) maxGPA = "4>";
+
+  return (
+    <tbody className="text-gray-600">
+      <tr className="bg-gray-100 border-2 border-slate-400">
+        <td className="text-gray-200 font-semibold bg-slate-600 p-2">
+          {programAdi} {isIngilizce ? "(İngilizce)" : ""}
+        </td>
+        <td className="text-gray-800 bg-slate-300 text-center">{yilAdi}</td>
+        <td className="text-gray-800 bg-slate-300 text-center">{kontenjan}</td>
+        <td className="text-gray-800 bg-slate-300 text-center">{yerlesen}</td>
+        <td className="text-gray-800 bg-slate-300 text-center">{minGPA}</td>
+        <td className="text-gray-800 bg-slate-300 text-center">{maxGPA}</td>
+      </tr>
+    </tbody>
   );
 }
